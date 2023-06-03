1141c4e7f962e903f07ff88d997a5e6ff11d19f7
Rafael Blumm <rafaelfloresblumm@gmail.com>
2023-06-03 20:41:29 -0300

    Melhorias na legibilidade. Arquivo só é salvo se houve alteração nos dados

--- src/java/ui/EstoqueUI.java
+++ src/java/ui/EstoqueUI.java
@@ -7,76 +7,80 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EstoqueUI {
+    private static final int OP_INSERIR = 1;
+    private static final int OP_ADICIONAR_QNT = 2;
+    private static final int OP_RETIRAR_QNT = 3;
+    private static final int OP_LISTAR = 4;
+    private static final int OP_LISTAR_ABAIXO_ESTOQUE = 5;
+    private static final int OP_SAIR = 0;
+    private static final int OP_INVALIDA = -1;
+
     private static Estoque estoque;
     private static CSVHandler csvHandler;
+    private static Scanner scanner;
+    private static boolean estoqueFoiModificado;
 
     public static void iniciar() {
         estoque = new Estoque();
         csvHandler = new CSVHandler(estoque);
+        scanner = new Scanner(System.in);
+        estoqueFoiModificado = false;
 
         // Carregar os dados do estoque a partir do arquivo CSV
-        if (csvHandler.leRegistroCsv()) {
+        if (csvHandler.leRegistro())
             System.out.println("Dados do estoque foram carregados do arquivo CSV.");
-        } else {
+        else
             System.out.println("Não foi possível carregar os dados do estoque do arquivo CSV.");
-        }
 
-        exibirMenu();
+        int opcao;
+        do {
+            exibeMenu();
+            opcao = aceitaOperacao();
+            executaOperacao(opcao);
+        } while (opcao != OP_SAIR);
     }
 
-    private static void exibirMenu() {
-        Scanner scanner = new Scanner(System.in);
-        int opcao;
+    private static void exibeMenu() {
+        System.out.println("\n==== Menu ====");
+        System.out.println("1. Inserir produto");
+        System.out.println("2. Adicionar quantidade de produtos no estoque");
+        System.out.println("3. Retirar quantidade de produtos no estoque");
+        System.out.println("4. Listar todos os produtos");
+        System.out.println("5. Listar produtos abaixo do estoque");
+        System.out.println("0. Sair");
+    }
 
+    private static int aceitaOperacao() {
+        int opcao;
         do {
             try {
-                System.out.println("\n==== Menu ====");
-                System.out.println("1. Inserir produto");
-                System.out.println("2. Adicionar quantidade de produtos no estoque");
-                System.out.println("3. Retirar quantidade de produtos no estoque");
-                System.out.println("4. Listar todos os produtos");
-                System.out.println("5. Listar produtos abaixo do estoque");
-                System.out.println("0. Sair");
-
                 System.out.print("Digite a opção desejada: ");
                 opcao = scanner.nextInt();
                 scanner.nextLine();
-
-                switch (opcao) {
-                    case 1:
-                        inserirProduto(scanner);
-                        break;
-                    case 2:
-                        adicionarQuantidade(scanner);
-                        break;
-                    case 3:
-                        retirarQuantidade(scanner);
-                        break;
-                    case 4:
-                        listarProdutos();
-                        break;
-                    case 5:
-                        listarProdutosAbaixoEstoque();
-                        break;
-                    case 0:
-                        sair();
-                        break;
-                    default:
-                        System.out.println("Opção inválida. Digite um número válido.");
-                        break;
-                }
             } catch (InputMismatchException e) {
                 System.out.println("Digite um valor válido!");
                 scanner.next();
-                opcao = -1;
+                opcao = OP_INVALIDA;
             }
-        } while (opcao != 0);
+        } while (opcao == OP_INVALIDA);
+
+        return opcao;
     }
 
+    private static void executaOperacao(int opcao) {
+        switch (opcao) {
+            case OP_INSERIR -> inserirProduto();
+            case OP_ADICIONAR_QNT -> adicionarQuantidade();
+            case OP_RETIRAR_QNT -> retirarQuantidade();
+            case OP_LISTAR -> listarProdutos();
+            case OP_LISTAR_ABAIXO_ESTOQUE -> listarProdutosAbaixoEstoque();
+            case OP_SAIR -> sair();
+            default -> System.out.println("Opção inválida. Digite um número válido.");
+        }
+    }
 
-    private static void inserirProduto(Scanner scanner) {
-        System.out.print("Digite o código do produto: ");
-        String codigo = scanner.nextLine();
+    private static void inserirProduto() {
+        String codigo = obterCodigo();
 
         if (estoque.consultaProduto(codigo) != null) {
             System.out.println("O produto já existe no estoque.");
@@ -85,23 +89,19 @@ public class EstoqueUI {
 
         System.out.print("Digite o nome do produto: ");
         String nome = scanner.nextLine();
-
-        int quantidade = obterQuantidade(scanner, "Digite a quantidade de produtos: ");
-
-        int quantidadeMinima = obterQuantidade(scanner, "Digite a quantidade mínima do estoque: ");
+        int quantidade = obterQuantidade("Digite a quantidade de produtos: ");
+        int quantidadeMinima = obterQuantidade("Digite a quantidade mínima do estoque: ");
 
         Produto produto = new Produto(codigo, nome, quantidade, quantidadeMinima);
         if (estoque.insereProduto(produto)) {
             System.out.println("Produto inserido no estoque com sucesso.");
-        } else {
+            estoqueFoiModificado = true;
+        } else
             System.out.println("Falha ao inserir produto no estoque.");
-        }
     }
 
-    private static void adicionarQuantidade(Scanner scanner) {
-        System.out.print("Digite o código do produto: ");
-        String codigo = scanner.nextLine();
-
+    private static void adicionarQuantidade() {
+        String codigo = obterCodigo();
         Produto produto = estoque.consultaProduto(codigo);
 
         if (produto == null) {
@@ -109,19 +109,17 @@ public class EstoqueUI {
             return;
         }
 
-        int quantidade = obterQuantidade(scanner, "Digite a quantidade a ser adicionada: ");
+        int quantidade = obterQuantidade("Digite a quantidade a ser adicionada: ");
 
         if (estoque.adicionaQuantidade(codigo, quantidade)) {
             System.out.println("Quantidade adicionada ao estoque com sucesso.");
-        } else {
+            estoqueFoiModificado = true;
+        } else
             System.out.println("Falha ao adicionar quantidade ao estoque.");
-        }
     }
 
-    private static void retirarQuantidade(Scanner scanner) {
-        System.out.print("Digite o código do produto: ");
-        String codigo = scanner.nextLine();
-
+    private static void retirarQuantidade() {
+        String codigo = obterCodigo();
         Produto produto = estoque.consultaProduto(codigo);
 
         if (produto == null) {
@@ -129,13 +127,13 @@ public class EstoqueUI {
             return;
         }
 
-        int quantidade = obterQuantidade(scanner, "Digite a quantidade a ser retirada: ");
+        int quantidade = obterQuantidade("Digite a quantidade a ser retirada: ");
 
         if (estoque.removeQuantidade(codigo, quantidade)) {
             System.out.println("Quantidade retirada do estoque com sucesso.");
-        } else {
+            estoqueFoiModificado = true;
+        } else
             System.out.println("Falha ao retirar quantidade do estoque.");
-        }
     }
 
     private static void listarProdutos() {
@@ -162,7 +160,23 @@ public class EstoqueUI {
         }
     }
 
-    private static int obterQuantidade(Scanner scanner, String mensagem) {
+    private static String obterCodigo() {
+        String codigo;
+
+        while (true) {
+            System.out.println("Digite o código do produto: ");
+            codigo = scanner.nextLine().trim();
+
+            if (codigo.matches("[0-9]+"))
+                break;
+
+            System.out.println("Código inválido. Somente números são permitidos.");
+        }
+
+        return codigo;
+    }
+
+    private static int obterQuantidade(String mensagem) {
         int quantidade;
 
         while (true) {
@@ -187,11 +201,9 @@ public class EstoqueUI {
 
     private static void sair() {
         // Gravar os dados do estoque no arquivo CSV
-        if (csvHandler.gravaRegistroCsv()) {
-            System.out.println("Dados do estoque foram salvos no arquivo CSV.");
-        } else {
-            System.out.println("Não foi possível salvar os dados do estoque no arquivo CSV.");
-        }
+        if (estoqueFoiModificado)
+            System.out.println(csvHandler.gravaRegistro() ? "Dados do estoque foram salvos no arquivo CSV." :
+                    "Não foi possível salvar os dados do estoque no arquivo CSV.");
 
         System.out.println("Encerrando o programa. Até logo!");
     }
